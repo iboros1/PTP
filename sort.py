8127b351fc896016519411358a2c8b1c157954f2
Istvan Boros <istvan.boros@3pillarglobal.com>
2017-03-23 18:11:42 +0200

    1 left

---
+++ sort.py
@@ -0,0 +1,21 @@
+# Given a file containing a list of dictionaries implement a sorting algorithm (of your choosing) that will sort the list
+# based on the dictionary keys.
+# The dictionary keys will contain alphabetic characters while the values will be integers.
+# The rule for comparing dictionaries between them is:
+# if the value of the dictionary with the lowest alphabetic key is lower than the value of the other dictionary with the
+# lowest alphabetic key, then the first dictionary is smaller than the second.
+# if the two values specified in the previous rule are equal reapply the algorithm ignoring the current key.
+#
+# The input is a file containing the list of dictionaries. Each dictionary key value is specified on the same line in the
+# form <key> <whitespace> <value>. Each list item is split by an empty row. The output is a file containing a list of
+# integers specifying the dictionary list in sorted order. Each integer identifies a dictionary in the order they were
+# received in the input file.
+from file import a
+
+
+
+
+def sorting(a):
+    print(a)
+
+sorting(a)
